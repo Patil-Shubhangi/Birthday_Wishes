1224aa3fd2fac7d704f23ac34579aadd1f3dc719
Patil-Shubhangi <161196110+Patil-Shubhangi@users.noreply.github.com>
2024-02-25 22:34:43 +0530

    Add files via upload

---
+++ Birthday_Wish_for_CS_Student's/birthday_wishes/script.js
@@ -0,0 +1,8 @@
+var a = new Audio("https://dl.dropbox.com/s/e93f5kxsmtankf3/Best%20Happy%20Birthday%20To%20You%20_%20Happy%20Birthday%20Songs%20Remix%202020%28M4A_128K%29_1.m4a?dl=0");
+window.addEventListener('click', () => {
+    a.play();
+    a.mute();
+});
+confirm("Happy Birthday Dear..🥳💫🎊 A small try🧗‍♀️ to express my gratitude🤗 It is quite simple😕😝and boring🤭 as I am a beginner🙁👻")
+alert("HAVE A FABULOUS BIRTHDAY!!(click on screen for music🥳🎊🎉!!)")
+alert("I hope you like it😍♥️!")
